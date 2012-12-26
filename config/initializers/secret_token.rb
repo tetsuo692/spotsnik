8f07c619e05f7e24e77acfaa33396e56d31a6dc3
Guillaume belleguic <guillaume@belleguic.com>
2012-12-26 09:51:39 +0100

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Spotsnik::Application.config.secret_token = '2ff3b5e6af6faf991f0dd4b0930c90afcceeb728c271679f5ea4299a112093d7069c166e173454831884f6de3b665d97822a665cf73de2b3f444a55ae9cce224'
